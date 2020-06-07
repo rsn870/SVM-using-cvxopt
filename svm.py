06b42e5f78e241cc4a0f7e00563ed38fa8a3f74e
rsn870 <50112766+rsn870@users.noreply.github.com>
2020-06-07 15:11:21 +0530

    Adding code

---
+++ svm.py
@@ -0,0 +1,265 @@
+import numpy as np  
+from numpy import linalg
+import cvxopt
+from cvxopt import solvers
+import pylab as pl
+
+
+def linear_kernel(x1, x2):
+    return np.dot(x1, x2)
+
+def polynomial_kernel(x, y, p=3):
+    return (1 + np.dot(x, y)) ** p            #Polynomial kernel chose p in the range 3-5 typically for good fit 
+
+def gaussian_kernel(x, y, sigma=5.0):
+    return np.exp(-linalg.norm(x-y)**2 / (2 * (sigma ** 2)))   #Gaussian RBF
+
+
+
+#Soft Margin SVM
+class SVM() :
+	def __init(self , kernel = linear_kernel , C = None) :
+		self.C = C
+		self.kernel = kernel
+		if self.C != None :
+			self.C = float(self.C)
+
+	def forward(self , X , y):
+
+		samples , features = X.shape #unzip 
+		
+		#Initialise Gram Matrix
+
+		K = np.zeros((samples, samples))
+        for i in range(samples):
+            for j in range(amples):
+                K[i,j] = self.kernel(X[i], X[j])  #Kernelise
+
+        P = cvxopt.matrix(np.outer(y,y) * K)
+        q = cvxopt.matrix(np.ones(samples) * -1)
+        A = cvxopt.matrix(y, (1, samples))
+        b = cvxopt.matrix(0.0) 
+
+        if self.C is None :
+        	G = cvxopt.matrix(np.diag(np.ones(samples) * -1))
+        	h = cvxopt.matrix(np.zeros(samples))
+
+        else :
+        	tmp = np.diag(np.ones(samples) * -1)
+        	tmp2 = np.identity(samples)
+        	G = cvxopt.matrix(np.vstack(tmp,tmp2))
+        	tmp = np.zeros(samples)
+        	tmp2 = np.ones(samples) * self.C
+        	h = cvxopt.matrix(np.hstack(tmp,tmp2))
+
+        #Solve the Quadratic Programming Problem
+
+        solution = cvxopt.solvers(P,q,G,h,A,b)
+
+        #Now to use lagrange multipliers 
+
+        l = np.ravel(solution['x'])
+
+        # Support vectors have non zero lagrange multipliers
+        sv = l > 1e-5
+        ind = np.arange(len(l))[sv]
+        self.l = l[sv]
+        self.sv = X[sv]
+        self.sv_y = y[sv]
+        print("%d support vectors out of %d points" % (len(self.l), samples))
+
+        # Intercept   
+        self.b = 0           #initialise b as per reqmts
+        for n in range(len(self.l)):
+            self.b += self.sv_y[n]
+            self.b -= np.sum(self.l * self.sv_y * K[ind[n],sv])
+        self.b /= len(self.l)
+
+        # Weight vector
+        if self.kernel == linear_kernel:
+            self.w = np.zeros(features) #Initialse as per requirements use np.random 
+            for n in range(len(self.a)):
+                self.w += self.l[n] * self.sv_y[n] * self.sv[n]
+        else:
+            self.w = None
+
+    def evaluate(self , X) :
+    	if self.w is not None:
+            return np.dot(X, self.w) + self.b
+        else:
+            y_predict = np.zeros(len(X))
+            for i in range(len(X)):
+                s = 0
+                for a, sv_y, sv in zip(self.a, self.sv_y, self.sv):
+                    s += a * sv_y * self.kernel(X[i], sv)
+                y_predict[i] = s
+            return y_predict + self.b
+
+    def prediction(self , X) :
+    	return np.sign(self.evaluate(X))
+
+
+
+def gen_lin_separable_data():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[0.8, 0.6], [0.6, 0.8]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+
+def gen_non_lin_separable_data():
+        mean1 = [-1, 2]
+        mean2 = [1, -1]
+        mean3 = [4, -4]
+        mean4 = [-4, 4]
+        cov = [[1.0,0.8], [0.8, 1.0]]
+        X1 = np.random.multivariate_normal(mean1, cov, 50)
+        X1 = np.vstack((X1, np.random.multivariate_normal(mean3, cov, 50)))
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 50)
+        X2 = np.vstack((X2, np.random.multivariate_normal(mean4, cov, 50)))
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+def gen_lin_separable_overlap_data():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[1.5, 1.0], [1.0, 1.5]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+def split_train(X1, y1, X2, y2):
+        X1_train = X1[:90]
+        y1_train = y1[:90]
+        X2_train = X2[:90]
+        y2_train = y2[:90]
+        X_train = np.vstack((X1_train, X2_train))
+        y_train = np.hstack((y1_train, y2_train))
+        return X_train, y_train
+
+def split_test(X1, y1, X2, y2):
+        X1_test = X1[90:]
+        y1_test = y1[90:]
+        X2_test = X2[90:]
+        y2_test = y2[90:]
+        X_test = np.vstack((X1_test, X2_test))
+        y_test = np.hstack((y1_test, y2_test))
+        return X_test, y_test
+
+def plot_margin(X1_train, X2_train, clf):
+        def f(x, w, b, c=0):
+            # given x, return y such that [x,y] in on the line
+            # w.x + b = c
+            return (-w[0] * x - b + c) / w[1]
+
+        pl.plot(X1_train[:,0], X1_train[:,1], "ro")
+        pl.plot(X2_train[:,0], X2_train[:,1], "bo")
+        pl.scatter(clf.sv[:,0], clf.sv[:,1], s=100, c="g")
+
+        # w.x + b = 0
+        a0 = -4; a1 = f(a0, clf.w, clf.b)
+        b0 = 4; b1 = f(b0, clf.w, clf.b)
+        pl.plot([a0,b0], [a1,b1], "k")
+
+        # w.x + b = 1
+        a0 = -4; a1 = f(a0, clf.w, clf.b, 1)
+        b0 = 4; b1 = f(b0, clf.w, clf.b, 1)
+        pl.plot([a0,b0], [a1,b1], "k--")
+
+        # w.x + b = -1
+        a0 = -4; a1 = f(a0, clf.w, clf.b, -1)
+        b0 = 4; b1 = f(b0, clf.w, clf.b, -1)
+        pl.plot([a0,b0], [a1,b1], "k--")
+
+        pl.axis("tight")
+        pl.show()
+
+def plot_contour(X1_train, X2_train, clf):
+        pl.plot(X1_train[:,0], X1_train[:,1], "ro")
+        pl.plot(X2_train[:,0], X2_train[:,1], "bo")
+        pl.scatter(clf.sv[:,0], clf.sv[:,1], s=100, c="g")
+
+        X1, X2 = np.meshgrid(np.linspace(-6,6,50), np.linspace(-6,6,50))
+        X = np.array([[x1, x2] for x1, x2 in zip(np.ravel(X1), np.ravel(X2))])
+        Z = clf.project(X).reshape(X1.shape)
+        pl.contour(X1, X2, Z, [0.0], colors='k', linewidths=1, origin='lower')
+        pl.contour(X1, X2, Z + 1, [0.0], colors='grey', linewidths=1, origin='lower')
+        pl.contour(X1, X2, Z - 1, [0.0], colors='grey', linewidths=1, origin='lower')
+
+        pl.axis("tight")
+        pl.show()
+
+def test_linear():
+        X1, y1, X2, y2 = gen_lin_separable_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM()
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print("%d out of %d predictions correct" % (correct, len(y_predict)))
+
+        plot_margin(X_train[y_train==1], X_train[y_train==-1], clf)
+
+def test_non_linear():
+        X1, y1, X2, y2 = gen_non_lin_separable_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM(polynomial_kernel)
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print("%d out of %d predictions correct" % (correct, len(y_predict)))
+
+        plot_contour(X_train[y_train==1], X_train[y_train==-1], clf)
+
+def test_soft():
+        X1, y1, X2, y2 = gen_lin_separable_overlap_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM(C=1000.1)
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print("%d out of %d predictions correct" % (correct, len(y_predict)))
+
+        plot_contour(X_train[y_train==1], X_train[y_train==-1], clf)
+
+
+
+def main() :
+	test_soft()
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
